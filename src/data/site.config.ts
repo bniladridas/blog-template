c69aa47d413f3b4bc00e8ba7b9805146f8afa4a9
Niladri Das <125604915+bniladridas@users.noreply.github.com>
2025-03-12 19:19:08 +0000

    changes

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -10,10 +10,10 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	site: 'https://blog-template-gray.vercel.app/', // Write here your website url
-	author: 'Your Name', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
+	site: '', // Write here your website url
+	author: 'Niladri Das', // Site author
+	title: 'Full Stack Developer', // Site title.
+	description: 'Life in a mirror of a software engineer.', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
